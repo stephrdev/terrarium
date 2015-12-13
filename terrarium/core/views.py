8063f31006ec09fd5b701b25cda646a7c9a417f5
Stephan Jaekel <steph@rdev.info>
2015-12-13 16:05:56 +0100

    Revert "Debugging some performance issues when running on raspberry pi."
    This reverts commit e4807a4df560fc519c98e0082a879f38e12408ab.

--- terrarium/core/views.py
+++ terrarium/core/views.py
@@ -1,28 +1,5 @@
-from django.http import JsonResponse
-from django.views.generic import TemplateView, View
-from django.views.generic.list import MultipleObjectMixin
-
-from didadata.models import Record
+from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
-
-
-class RecordsApiView(MultipleObjectMixin, View):
-    model = Record
-    paginate_by = 250
-
-    def get_queryset(self):
-        qset = super().get_queryset()
-        if self.request.GET.get('metric', ''):
-            qset = qset.filter(metric__name__in=self.request.GET['metric'].split(','))
-        return qset
-
-    def get(self, request, *args, **kwargs):
-        paginator, page, queryset, is_paginated = self.paginate_queryset(
-            self.get_queryset(), self.paginate_by)
-        return JsonResponse({'results': [
-            {'metric': obj.metric.name, 'timestamp': obj.timestamp, 'value': obj.value}
-            for obj in queryset
-        ]})
--- terrarium/urls.py
+++ terrarium/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from terrarium.core.views import HomeView, RecordsApiView
+from terrarium.core.views import HomeView
 
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('didadata.urls')),
-    url(r'^api2/records/', RecordsApiView.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
