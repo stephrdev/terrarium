2b1a2c4f0e35a703d7ea39b7f3656ac62ca9cdbf
Stephan Jaekel <steph@rdev.info>
2015-12-13 20:14:01 +0100

    Added missing parser for form input.

--- terrarium/conf/global_settings.py
+++ terrarium/conf/global_settings.py
@@ -124,6 +124,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'drf_ujson.parsers.UJSONParser',
+        'rest_framework.parsers.FormParser',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 250,
