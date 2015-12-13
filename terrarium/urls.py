e4807a4df560fc519c98e0082a879f38e12408ab
Stephan Jaekel <steph@rdev.info>
2015-12-13 15:11:07 +0100

    Debugging some performance issues when running on raspberry pi.

--- terrarium/urls.py
+++ terrarium/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from terrarium.core.views import HomeView
+from terrarium.core.views import HomeView, RecordsApiView
 
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('didadata.urls')),
+    url(r'^api2/records/', RecordsApiView.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
