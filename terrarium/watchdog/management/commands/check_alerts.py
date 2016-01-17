7c7656a1a4643164b9fa53ef135887642f522a8a
Benjamin Banduhn <benjamin.banduhn@moccu.com>
2016-01-17 20:34:39 +0100

    remove unused translation and imports that are not used anymore

--- terrarium/watchdog/management/commands/check_alerts.py
+++ terrarium/watchdog/management/commands/check_alerts.py
@@ -1,8 +1,6 @@
 import logging
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.utils import translation
 
 from terrarium.watchdog.models import Watchdog
 
@@ -13,8 +11,6 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        translation.activate(settings.LANGUAGE_CODE)
-
         logger.debug('>>  Checking alerts...')
 
         for data in Watchdog.objects.all():
